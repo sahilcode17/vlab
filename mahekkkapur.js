f5eca5c176390619d06942e22a3ae66fb975a7b5
ishaputhige <44339577+ishaputhige@users.noreply.github.com>
2020-05-25 17:27:14 +0530

    changes 2

--- mahekkkapur.js
+++ mahekkkapur.js
@@ -1,143 +1,147 @@
-button 1
- arr=[0,1,0,0,0];
+var button1 = document.querySelector('#b1');
+var button2 = document.querySelector('#b2');
+var button3 = document.querySelector('#b3');
+var button4 = document.querySelector('#b4');
+var button5 = document.querySelector('#b5');
+var button6 = document.querySelector('#b6');
+var button7 = document.querySelector('#b7');
+var button8 = document.querySelector('#b8');
+var button9 = document.querySelector('#b9');
+var button10 = document.querySelector('#b10');
 
-button 2
+alert("Let's start, select the first five frames to be sent");
 
-if(count==2)
-                {
-                    arr=[0,1,0,0,0];
-                }
-                else
-                {
-                    arr=[0,0,1,0,0];
-                }
-                
-button 3
+// import {myMove} from 'panda';
+//last_call_yellow();
+var flag = 0;
+var arr = [1, 1, 1, 1, 1, 0, 0, 0, 0, 0];
+var count = 0;
 
-arr=[0,0,0,1,0];
-                
-button 4
+if (flag == 0) {
+    button1.onclick = function() {
+        if (!(arr[0])) {
+            wrongclick();
+            button1.style.backgroundColor = "red";
+            flag = 0;
+        } else {
+            button1.style.backgroundColor = "green";
+            count += 1;
+        }
 
-  if(count==5)
-                {
-                    arr=[0,0,0,1,0];
-                }
-                else
-                {
-                    arr=[0,0,0,0,1];
-                }
-                
-button 5
+    };
+    button2.onclick = function() {
+        if (!(arr[1])) {
+            wrongclick();
+            button2.style.backgroundColor = "red";
+            flag = 0;
+        } else {
+            button2.style.backgroundColor = "green";
+            count = count + 1;
+        }
 
-arr=[0,0,0,0,0];
+    };
+
+    button3.onclick = function() {
+        if (!(arr[2])) {
+            wrongclick();
+            button3.style.backgroundColor = "red";
+            flag = 0;
+        } else {
+            count += 1;
+            button3.style.backgroundColor = "green";
+        }
+
+    };
+
+    button4.onclick = function() {
+        if (!(arr[3])) {
+            wrongclick();
+            button4.style.backgroundColor = "red";
+            flag = 0;
+        } else {
+            button4.style.backgroundColor = "green";
+            count = count + 1;
 
 
-mahekkkapur.js
+        }
+
+    };
+
+    button5.onclick = function() {
+        if (!(arr[4])) {
+            wrongclick();
+            button5.style.backgroundColor = "red";
+            flag = 0;
+        } else {
+            button5.style.backgroundColor = "green";
+            count = count + 1;
+
+        }
+
+    };
+
+    button6.onclick = function() {
+        if (!(arr[5])) {
+            wrongclick();
+            button6.style.backgroundColor = "red";
+            flag = 0;
+        } else {
+            button6.style.backgroundColor = "green";
+            count += 1;
+        }
+
+    };
+    button7.onclick = function() {
+        if (!(arr[6])) {
+            wrongclick();
+            button7.style.backgroundColor = "red";
+            flag = 0;
+        } else {
+            button7.style.backgroundColor = "green";
+            count = count + 1;
+        }
+
+    };
+
+    button8.onclick = function() {
+        if (!(arr[7])) {
+            wrongclick();
+            button8.style.backgroundColor = "red";
+            flag = 0;
+        } else {
+            count += 1;
+            button8.style.backgroundColor = "green";
+        }
+
+    };
+
+    button9.onclick = function() {
+        if (!(arr[8])) {
+            wrongclick();
+            button9.style.backgroundColor = "red";
+            flag = 0;
+        } else {
+            button9.style.backgroundColor = "green";
+            count = count + 1;
 
 
-//case 1
+        }
 
-function mymove_k1() {
-    alert("frame sent");
-    myMove1_1();
-    
-    setTimeout(myMove2_1,1000);
-}
+    };
 
-function myMove2_1() {
-    alert("ack sent");
-    var elem = document.getElementById("animate"); 
-    
+    button10.onclick = function() {
+        if (!(arr[9])) {
+            wrongclick();
+            button10.style.backgroundColor = "red";
+            flag = 0;
+        } else {
+            button10.style.backgroundColor = "green";
+            count = count + 1;
 
-    var posx = 150;
-    var posy = 150;
-    var id = setInterval(frame, 5);
-    function frame() {
-      if (posx == 0 & posy == 300) {
-        clearInterval(id);
-      } else {
-        posx--;
-        posy++; 
-        elem.style.top = posy + "px"; 
-        elem.style.left = posx + "px"; 
-         
-        
-      }
-    }
-    
-  }
-  function myMove1_1() {
-    var elem = document.getElementById("animate");   
-    var posx = 0;
-    var posy = 0;
-    var id = setInterval(frame, 5);
-    function frame() {
-      if (posx == 150 & posy == 150) {
-        clearInterval(id);
-      } else {
-        posx++;
-        posy++; 
-        elem.style.top = posy + "px"; 
-        elem.style.left = posx + "px"; 
-         
-        
-      }
-    }
-  }
+        }
 
-  //case 2
+    };
 
-  function mymove_k2() {
-    alert("frame sent");
-    myMove1_1();
-    
-    setTimeout(myMove2_2,1000);
-}
+    if (count == )
 
-function myMove2_2() {
-    alert("ack sent");
-    var elem = document.getElementById("animate"); 
-    var posx = 150;
-    var posy = 150;
-    var id = setInterval(frame, 5);
-    function frame() {
-      if (posx == 75 & posy == 225) {
-        clearInterval(id);
-      } else {
-        posx--;
-        posy++; 
-        elem.style.top = posy + "px"; 
-        elem.style.left = posx + "px"; 
-         
-        
-      }
-    }
-    
-  }
-
-  //case 3
-  
-  function mymove_k3() {
-    alert("frame sent");
-    myMove1_3();
-    
-}
-
-  function myMove1_3() {
-    var elem = document.getElementById("animate");   
-    var posx = 0;
-    var posy = 0;
-    var id = setInterval(frame, 5);
-    function frame() {
-      if (posx == 75 & posy == 75) {
-        clearInterval(id);
-      } else {
-        posx++;
-        posy++; 
-        elem.style.top = posy + "px"; 
-        elem.style.left = posx + "px"; 
-         
-        
-      }
-    }
-  }
+};
--- panda.js
+++ panda.js
@@ -1,83 +1,78 @@
+// //       //export{myMove};
+
+// //       // function one(callback) {
+// //       //   setTimeout(function() {
+// //       //     console.log("first function executed");
+// //       //     callback();
+// //       //   }, 3000);
+// //       // }
+
+// //       // function two() {
+// //       //   console.log("second function executed");
+// //       // }
+
+// //       myMove(myMove2)
+
+// var myGamePiece;
+// var myGamePiece123;
 
 
+// function startGame() {
+//     myGamePiece = new component(30, 30, "red", 10, 120);
+//     myGamePiece123=new component(30, 30, "yellow", 10, 120);
+//     myGameArea.start();
+// }
+
+// var myGameArea = {
+//     canvas : document.createElement("canvas"),
+//     start : function() {
+//         this.canvas.width = 480;
+//         this.canvas.height = 270;
+//         this.context = this.canvas.getContext("2d");
+//         document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+//         this.interval = setInterval(updateGameArea, 20);
+//     },
+//     clear : function() {
+//         this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+//     }
+// }
 
 
+// function component(width, height, color, x, y) {
+//   this.width = width;
+//   this.height = height;
+//   this.speedX = 0;
+//   this.speedY = 0;
+//   this.x = x;
+//   this.y = y;
+//   this.update = function() {
+//     ctx = myGameArea.context;
+//     ctx.fillStyle = color;
+//     ctx.fillRect(this.x, this.y, this.width, this.height);
+//   }
+//   this.newPos = function() {
+//     this.x += this.speedX;
+//     this.y += this.speedY;
+//   }
+// }
 
-//       //export{myMove};
-
-//       // function one(callback) {
-//       //   setTimeout(function() {
-//       //     console.log("first function executed");
-//       //     callback();
-//       //   }, 3000);
-//       // }
-      
-//       // function two() {
-//       //   console.log("second function executed");
-//       // }
-      
-//       myMove(myMove2)
-
-var myGamePiece;
-var myGamePiece123;
-
-
-function startGame() {
-    myGamePiece = new component(30, 30, "red", 10, 120);
-    myGamePiece123=new component(30, 30, "yellow", 10, 120);
-    myGameArea.start();
-}
-
-var myGameArea = {
-    canvas : document.createElement("canvas"),
-    start : function() {
-        this.canvas.width = 480;
-        this.canvas.height = 270;
-        this.context = this.canvas.getContext("2d");
-        document.body.insertBefore(this.canvas, document.body.childNodes[0]);
-        this.interval = setInterval(updateGameArea, 20);
-    },
-    clear : function() {
-        this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
-    }
-}
+// function updateGameArea() {
+//   myGameArea.clear();
+//   myGamePiece.newPos();
+//   myGamePiece.update();
+//   myGamePiece123.newPos();
+//   myGamePiece1.update();
+// }
 
 
-function component(width, height, color, x, y) {
-  this.width = width;
-  this.height = height;
-  this.speedX = 0;
-  this.speedY = 0;
-  this.x = x;
-  this.y = y;
-  this.update = function() {
-    ctx = myGameArea.context;
-    ctx.fillStyle = color;
-    ctx.fillRect(this.x, this.y, this.width, this.height);
-  }
-  this.newPos = function() {
-    this.x += this.speedX;
-    this.y += this.speedY;
-  }
-}
+// //var myVar = setInterval(moveup, 1000);
 
-function updateGameArea() {
-  myGameArea.clear();
-  myGamePiece.newPos();
-  myGamePiece.update();
-  myGamePiece123.newPos();
-  myGamePiece1.update();
-}
+// function moveright() {
 
-
-//var myVar = setInterval(moveup, 1000);
-
-function moveright() {
-  
-  myGamePiece.speedX = 1; 
-  myGamePiece123.speedX = 0; 
-}
-function moveleft() {
-  myGamePiece.speedX =0; 
-  myGamePiece123.speedX = 1; 
-}
+//   myGamePiece.speedX = 1; 
+//   myGamePiece123.speedX = 0; 
+// }
+// function moveleft() {
+//   myGamePiece.speedX =0; 
+//   myGamePiece123.speedX = 1; 
+// }
