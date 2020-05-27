0949e6c95a43c33949374ade2f2cf0d0657ca7f2
sahilinjaipur <sahilinjaipur17@gmail.com>
2020-05-27 12:07:15 +0530

    karchange

--- selectiverep.js
+++ selectiverep.js
@@ -1,4 +1,4 @@
-var button1 = document.get('#bt1');
+var button1 = document.getElementById('#bt1');
 var button2 = document.getElementById('#bt2');
 var button3 = document.getElementById('#bt3');
 var button4 = document.getElementById('#bt4');
@@ -8,10 +8,7 @@ var button7 = document.getElementById('#bt7');
 var button8 = document.getElementById('#bt8');
 var button9 = document.getElementById('#bt9');
 var button10 = document.getElementById('#bt10');
-var path2 = document.getElementById("line1frame")
-var path4 = document.getElementById("line2frame")
-var path6 = document.getElementById("line3frame")
-var path8 = document.getElementById("line4frame")
+
 var flag = 0;
 arr = [1, 1, 1, 1, 0, 0, 0, 0, 0, 0];
 var count = 0;
@@ -22,17 +19,21 @@ function wrongclick() {
 
 
 function f1() {
-    path2.style.animationPlayState = "running";
+    document.getElementById("line1frame").style.animationPlayState = "running";
+    document.getElementById("line2frame").style.animationPlayState = "running";
+    document.getElementById("line3frame").style.animationPlayState = "running";
+    document.getElementById("line4frame").style.animationPlayState = "running";
+    
 };
 
 
 function f2() {
     if (!(arr[1])) {
         wrongclick();
-        button2.style.background= 'red';
+        button2.style.background = 'red';
         flag = 0;
     } else {
-        button2.style.background= 'green';
+        button2.style.background = 'green';
         count += 1;
         alert(count.valueOf())
     }
@@ -41,10 +42,10 @@ function f2() {
 function f3() {
     if (!(arr[2])) {
         wrongclick();
-        button3.style.background= 'red';
+        button3.style.background = 'red';
         flag = 0;
     } else {
-        button3.style.background= 'green';
+        button3.style.background = 'green';
         count += 1;
         alert(count.valueOf())
     }
@@ -53,10 +54,10 @@ function f3() {
 function f4() {
     if (!(arr[3])) {
         wrongclick();
-        button4.style.background= 'red';
+        button4.style.background = 'red';
         flag = 0;
     } else {
-        button4.style.background= 'green';
+        button4.style.background = 'green';
         count += 1;
         alert(count.valueOf())
     }
@@ -65,10 +66,10 @@ function f4() {
 function f5() {
     if (!(arr[4])) {
         wrongclick();
-        button5.style.background= 'red';
+        button5.style.background = 'red';
         flag = 0;
     } else {
-        button5.style.background= 'green';
+        button5.style.background = 'green';
         count += 1;
 
     }
@@ -77,10 +78,10 @@ function f5() {
 function f6() {
     if (!(arr[5])) {
         wrongclick();
-        button6.style.background= 'red';
+        button6.style.background = 'red';
         flag = 0;
     } else {
-        button6.style.background= 'green';
+        button6.style.background = 'green';
         count += 1;
 
     }
@@ -89,10 +90,10 @@ function f6() {
 function f7() {
     if (!(arr[6])) {
         wrongclick();
-        button7.style.background= 'red';
+        button7.style.background = 'red';
         flag = 0;
     } else {
-        button7.style.background= 'green';
+        button7.style.background = 'green';
         count += 1;
 
     }
@@ -101,10 +102,10 @@ function f7() {
 function f8() {
     if (!(arr[7])) {
         wrongclick();
-        button8.style.background= 'red';
+        button8.style.background = 'red';
         flag = 0;
     } else {
-        button8.style.background= 'green';
+        button8.style.background = 'green';
         count += 1;
 
     }
@@ -113,10 +114,10 @@ function f8() {
 function f9() {
     if (!(arr[8])) {
         wrongclick();
-        button9.style.background= 'red';
+        button9.style.background = 'red';
         flag = 0;
     } else {
-        button9.style.background= 'green';
+        button9.style.background = 'green';
         count += 1;
 
     }
@@ -126,14 +127,11 @@ function f10() {
 
     if (!(arr[9])) {
         wrongclick();
-        button10.style.background= 'red';
+        button10.style.background = 'red';
         flag = 0;
     } else {
-        button10.style.background= 'green';
+        button10.style.background = 'green';
         count += 1;
 
     }
 };
-
-
-
