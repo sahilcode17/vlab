03ea10ec89638f04cebcb12106c087ac06cd8922
ishaputhige <44339577+ishaputhige@users.noreply.github.com>
2020-06-06 13:50:45 +0530

    post test complete

--- posttest.js
+++ posttest.js
@@ -1,6 +1,34 @@
 function result() {
-    var x = document.getElementsByName("q1").value;
-    if (x == a) {
-        document.getElementById("hello").style.color = "#ff0000";
+    if (document.getElementById('q12').checked) {
+        document.getElementById('q1c').innerHTML = "correct";
+    } else {
+        document.getElementById('q1w').innerHTML = "wrong ans, the correct option is B ";
+
+    }
+
+    if (document.getElementById('q22').checked) {
+        document.getElementById('q2c').innerHTML = "correct";
+    } else {
+        document.getElementById('q2w').innerHTML = "wrong ans, the correct option is B ";
+
+    }
+
+    if (document.getElementById('q33').checked) {
+        document.getElementById('q3c').innerHTML = "correct";
+    } else {
+        document.getElementById('q3w').innerHTML = "wrong ans, the correct option is C ";
+
+    }
+    if (document.getElementById('q44').checked) {
+        document.getElementById('q4c').innerHTML = "correct";
+    } else {
+        document.getElementById('q4w').innerHTML = "wrong ans, the correct option is D ";
+
+    }
+    if (document.getElementById('q51').checked) {
+        document.getElementById('q5c').innerHTML = "correct";
+    } else {
+        document.getElementById('q5w').innerHTML = "wrong ans, the correct option is A ";
+
     }
 }
