d2f1698df7ed14b52a7e1b3a2462b497c52e3d2c
sahilinjaipur <sahilinjaipur17@gmail.com>
2020-06-14 23:09:14 +0530

    error resolve

--- pretest.js
+++ pretest.js
@@ -3,7 +3,7 @@ function result1() {
         document.getElementById('q1p').innerHTML = "correct";
         document.getElementById('q1p').style.color="green"
     } else {
-        document.getElementById('q1p').innerHTML = "wrong ans, the correct option is B ";
+        document.getElementById('q1p').innerHTML = "wrong ans, the correct option is A ";
         document.getElementById('q1p').style.color="red"
 
     }
@@ -13,7 +13,7 @@ function result1() {
         document.getElementById('q2p').style.color="green"
         
     } else {
-        document.getElementById('q2p').innerHTML = "wrong ans, the correct option is B ";
+        document.getElementById('q2p').innerHTML = "wrong ans, the correct option is D ";
         document.getElementById('q2p').style.color="red"
     }
 
@@ -21,21 +21,21 @@ function result1() {
         document.getElementById('q3p').innerHTML = "correct";
         document.getElementById('q3p').style.color="green"
     } else {
-        document.getElementById('q3p').innerHTML = "wrong ans, the correct option is C ";
+        document.getElementById('q3p').innerHTML = "wrong ans, the correct option is B ";
         document.getElementById('q3p').style.color="red"
     }
     if (document.getElementById('q13').checked) {
         document.getElementById('q4p').innerHTML = "correct";
         document.getElementById('q4p').style.color="green"
     } else {
-        document.getElementById('q4p').innerHTML = "wrong ans, the correct option is D ";
+        document.getElementById('q4p').innerHTML = "wrong ans, the correct option is A ";
         document.getElementById('q4p').style.color="red"
     }
     if (document.getElementById('q18').checked) {
         document.getElementById('q5p').innerHTML = "correct";
         document.getElementById('q5p').style.color="green"
     } else {
-        document.getElementById('q5p').innerHTML = "wrong ans, the correct option is A ";
+        document.getElementById('q5p').innerHTML = "wrong ans, the correct option is B ";
         document.getElementById('q5p').style.color="red"
     }
 }
